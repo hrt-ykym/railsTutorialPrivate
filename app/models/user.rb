8abdf3a23edc4efd5f4c2a0cb721bca677e54cc3
hrt-ykym <haruto.yokoyama@pepabo.com>
2024-06-08 21:56:47 +0900

    一意性のテスト実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,6 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d-]+(\.[a-z\d-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX }
-end
-
-test 'email validation should accept valid addresses' do
-  valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
-                       first.last@foo.jp alice+bob@baz.cn]
-  valid_addresses.each do |valid_address|
-    @user.email = valid_address
-    assert @user.valid?, "#{valid_address.inspect} should be valid"
-  end
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
