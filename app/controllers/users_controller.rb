f0a005755cc124b15eab3396edbc3963032554d9
hrt-ykym <haruto.yokoyama@pepabo.com>
2024-06-11 23:04:25 +0900

    リダイレクトするようにした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @user=User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def new
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # 保存の成功をここで扱う。
+      redirect_to user_url(@user)
     else
       render 'new', status: :unprocessable_entity
     end
@@ -18,8 +18,8 @@ class UsersController < ApplicationController
 
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 end
