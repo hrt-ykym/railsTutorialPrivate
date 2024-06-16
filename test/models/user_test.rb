2db0026738791ef0d1136d9d9a6e525c734d8b6b
hrt-ykym <haruto.yokoyama@pepabo.com>
2024-06-16 14:48:28 +0900

    複数ブラウザ用のバグを修正

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com",
-                     password: "foobar", password_confirmation: "foobar")
+    @user = User.new(name: 'Example User', email: 'user@example.com',
+                     password: 'foobar', password_confirmation: 'foobar')
   end
 
   test 'should be valid' do
@@ -48,26 +48,30 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
-  test "email addresses should be unique" do
+  test 'email addresses should be unique' do
     duplicate_user = @user.dup
     @user.save
-    assert_not duplicate_user.valid? 
+    assert_not duplicate_user.valid?
   end
 
-  test "email addresses should be saved as lowercase" do
-    mixed_case_email = "Foo@ExAMPle.CoM"
+  test 'email addresses should be saved as lowercase' do
+    mixed_case_email = 'Foo@ExAMPle.CoM'
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
   end
-  
-  test "password should be present (nonblank)" do
-    @user.password = @user.password_confirmation = " " * 6
+
+  test 'password should be present (nonblank)' do
+    @user.password = @user.password_confirmation = ' ' * 6
     assert_not @user.valid?
   end
 
-  test "password should have a minimum length" do
-    @user.password = @user.password_confirmation = "a" * 5
+  test 'password should have a minimum length' do
+    @user.password = @user.password_confirmation = 'a' * 5
     assert_not @user.valid?
+  end
+
+  test 'authenticated? should return false for a user with nil digest' do
+    assert_not @user.authenticated?('')
   end
 end
