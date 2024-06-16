c083f87b0896aeb61a4ee7bd9ef06d9d8fab2bd5
hrt-ykym <haruto.yokoyama@pepabo.com>
2024-06-16 14:59:31 +0900

    remember_meテスト実装

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -94,3 +94,22 @@ class LogoutTest < Logout
     assert_redirected_to root_url
   end
 end
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
+  test 'login with remembering' do
+    log_in_as(@user, remember_me: '1')
+    assert_not cookies[:remember_token].blank?
+  end
+
+  test 'login without remembering' do
+    # Cookieを保存してログイン
+    log_in_as(@user, remember_me: '1')
+    # Cookieが削除されていることを検証してからログイン
+    log_in_as(@user, remember_me: '0')
+    assert cookies[:remember_token].blank?
+  end
+end
