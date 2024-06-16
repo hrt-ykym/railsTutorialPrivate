c4e4e2330271ce5215ccaad4b5bff5b1bed8a6c3
hrt-ykym <haruto.yokoyama@pepabo.com>
2024-06-16 13:01:13 +0900

    説明用のメモを追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:session][:password]) ## userが存在している(nilじゃない) かつ authenticateメソッドがtrueを返す場合
       reset_session # ログインの直前に必ずこれを書くこと
       log_in user
       redirect_to user
