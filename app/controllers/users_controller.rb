a4a847e742caaa5ea1ab5adb34c06523d3c9ebe1
hrt-ykym <haruto.yokoyama@pepabo.com>
2024-06-16 16:09:40 +0900

    friendly forwardを実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,6 +51,7 @@ class UsersController < ApplicationController
   def logged_in_user
     return if logged_in?
 
+    store_location
     flash[:danger] = 'Please log in.'
     redirect_to login_url, status: :see_other
   end
